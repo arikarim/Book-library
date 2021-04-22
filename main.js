682c71c2ba765c766ae849e44f91eb0653281773
arikarim <arikarim146@gmail.com>
2021-04-22 17:21:26 +0300

    Add Book constructor

--- main.js
+++ main.js
@@ -1,4 +1,90 @@
 let library = [];
 
+let bookList = document.getElementById('book-list');
+let submitButton = document.getElementById('submit-btn');
+let form = document.querySelector('form');
+form.style.display = 'none'
 
-let form = document.querySelector(form);
+let addBook = document.getElementById('add-book');
+// addBook.style.visibility = 'visible'
+let cancelBook = document.getElementById('close');
+// cancelBook.style.visibility = 'hidden'
+
+const displayform='--displayform';
+
+addBook.addEventListener('click',()=>{
+  form.style.display = 'block';
+})
+
+cancelBook.addEventListener('click',()=>{
+  form.style.display = 'none';
+})
+
+// function toggle() {
+//   form.style.visibility = form.style.visibility === 'hidden' ? 'visible' : 'hidden';
+//   addBook.style.visibility = form.style.visibility === 'hidden' ? 'visible' : 'hidden';
+//   cancelBook.style.visibility = addBook.style.visibility === 'visible' ? 'hidden' : 'visible';
+
+// }
+
+// addBook.addEventListener('click', toggle, false);
+// cancelBook.addEventListener('click', toggle, false);
+
+
+
+// create a book
+const addNewBook = (name,pages, read = false) => {
+  const bookObj = {bookname: name,pagesnum: pages,status: read};
+  library.push(bookObj);
+  addBookDom(bookObj);
+}
+
+const addBookDom =  (book) => {
+  // the div
+  const divBook = document.createElement('div');
+  divBook.classList.add('books');
+
+
+  // add book name 
+  const bookName = document.createElement('h2');
+  divBook.appendChild(bookName);
+
+  // add pages 
+  const bookPages = document.createElement('p');
+  divBook.appendChild(bookPages);
+
+  //create button
+  const readButton = document.createElement('button');
+  readButton.textContent = book.read;
+  readButton.classList.add('btn');
+  readButton.classList.add('btn-success');
+  divBook.appendChild(readButton);
+
+  // change readbutton status
+
+  readButton.addEventListener('click',(e) =>{
+    if (e.target.textContent == 'false') {
+      e.target.textContent = 'true'
+    } else
+    e.target.textContent = 'false'
+  })
+
+  bookList.appendChild(divBook);
+}
+
+submitButton.PreventDefault;
+
+// get the book info frm the form:
+
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const name = document.getElementById('name').value;
+  const pages = document.getElementById('pages').value;
+
+  addNewBook(name, pages);
+  form.reset();
+
+  form.style.visibility = 'hidden';
+
+})
